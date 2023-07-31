2517632c3b99ae90f2d5463cbc2dc474428d0575
venkat-kalyan-tekgile <venkat.kalyan@tekgile.com>
2023-07-31 10:44:52 +0530

    fixes

--- src/Components/ProjectDetails.js
+++ src/Components/ProjectDetails.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { TextField, Button, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import { TextField, Button } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const ProjectEditForm = () => {
   const navigate = useNavigate();
   const { projectId } = useParams();
 
-  
-  const planOptions = ['Plan A', 'Plan B', 'Plan C'];
-  const modelOptions = ['Model X', 'Model Y', 'Model Z'];
-  const parameterOptions = ['Parameter 1', 'Parameter 2', 'Parameter 3'];
-
   const initialProject = {
     name: '',
     description: '',
-    selectedPlan: '',
-    selectedModel: '',
-    selectedParameter: '',
   };
 
   const [project, setProject] = React.useState(initialProject);
@@ -30,20 +23,18 @@ const ProjectEditForm = () => {
   };
 
   const handleBackClick = () => {
-   
     navigate('/');
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
     console.log('Updated Project:', project);
     navigate('/');
   };
 
   return (
     <div style={{ padding: '20px' }}>
-      <Button onClick={handleBackClick} variant="contained" style={{ marginBottom: '10px' }}>
+      <Button onClick={handleBackClick} variant="text" startIcon={<ArrowBackIcon />} style={{ marginBottom: '10px' }}>
         Back
       </Button>
       <h2>Edit Project</h2>
@@ -67,48 +58,6 @@ const ProjectEditForm = () => {
           required
           margin="normal"
         />
-        <FormControl fullWidth margin="normal" required>
-          <InputLabel>Select Plan</InputLabel>
-          <Select
-            name="selectedPlan"
-            value={project.selectedPlan}
-            onChange={handleInputChange}
-          >
-            {planOptions.map((option) => (
-              <MenuItem key={option} value={option}>
-                {option}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <FormControl fullWidth margin="normal" required>
-          <InputLabel>Select Model</InputLabel>
-          <Select
-            name="selectedModel"
-            value={project.selectedModel}
-            onChange={handleInputChange}
-          >
-            {modelOptions.map((option) => (
-              <MenuItem key={option} value={option}>
-                {option}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <FormControl fullWidth margin="normal" required>
-          <InputLabel>Select Parameters</InputLabel>
-          <Select
-            name="selectedParameter"
-            value={project.selectedParameter}
-            onChange={handleInputChange}
-          >
-            {parameterOptions.map((option) => (
-              <MenuItem key={option} value={option}>
-                {option}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
         <Button type="submit" variant="contained" color="primary" style={{ marginTop: '10px' }}>
           Save
         </Button>
