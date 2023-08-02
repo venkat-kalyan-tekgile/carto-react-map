1a42eafae7f5a447240c53fad91d63d8cd81061d
venkat-kalyan-tekgile <venkat.kalyan@tekgile.com>
2023-08-02 19:11:05 +0530

    chgs

--- src/utils/carto.js
+++ src/utils/carto.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { setClient } from '@carto/react-core/src/utils/clientParameter';
+import { CartoLayer } from '@deck.gl/carto/typed';
 
 export const addCartoLayer = (map, setIsLoading) => {
     const CARTO_USERNAME = 'attainutbs@gmail.com';
@@ -50,3 +52,110 @@ export const addCartoLayer = (map, setIsLoading) => {
         setIsLoading(false);
       });
   };
+
+// import React from 'react';
+// import { setClient } from '@carto/react-core/src/utils/clientParameter';
+// import { CartoLayer } from '@deck.gl/carto/typed';
+
+//     const CARTO_USERNAME = 'attainutbs@gmail.com';
+//     const CARTO_API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhIjoiYWNfMWg2eGQxc2MiLCJqdGkiOiIwNDcxMjlkYiJ9.qdrpqYggDrYqaO1Oher5_lQn1pYy8TYdywexIm8cCGg'; // You need an API key for your CARTO account
+
+// setClient(CARTO_USERNAME, CARTO_API_KEY);
+
+// const CartoLayerComponent = () => {
+//   const cartoLayerConfig = {
+//     credentials: {
+//       apiKey: CARTO_API_KEY,
+//       username: CARTO_USERNAME,
+//       serverUrlTemplate: 'https://{user}.carto.com',
+//     },
+//     layerSource: {
+//       id: 'cell_towers_layer', // This is the layer ID you will use to add it to the map
+//       data: 'cell_towers_worldwide', // Table name from Carto
+//     },
+//     isVisible: true, // Set this to false if you want to hide the layer initially
+//   };
+
+//   return <CartoLayer {...cartoLayerConfig} />;
+// };
+
+// export default CartoLayerComponent;
+
+
+// import React from 'react';
+// import { setClient } from '@carto/react-core/src/utils/clientParameter';
+// import { DeckGL } from '@deck.gl/react';
+// import { CartoLayer } from '@deck.gl/carto/typed';
+
+// const CARTO_USERNAME = 'attainutbs@gmail.com';
+//     const CARTO_API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhIjoiYWNfMWg2eGQxc2MiLCJqdGkiOiIwNDcxMjlkYiJ9.qdrpqYggDrYqaO1Oher5_lQn1pYy8TYdywexIm8cCGg';
+
+// setClient(CARTO_USERNAME, CARTO_API_KEY);
+
+// const CartoLayerComponent = () => {
+//   const cartoLayerConfig = {
+//     credentials: {
+//       apiKey: CARTO_API_KEY,
+//       username: CARTO_USERNAME,
+//       serverUrlTemplate: 'https://{user}.carto.com',
+//     },
+//     layerSource: {
+//       id: 'cell_towers_layer', // This is the layer ID you will use to add it to the map
+//       data: 'cell_towers_worldwide', // Table name from Carto
+//     },
+//     isVisible: true, // Set this to false if you want to hide the layer initially
+//   };
+
+//   return (
+//     <DeckGL
+//       initialViewState={{
+//         latitude: 40.7128,
+//         longitude: -74.0060,
+//         zoom: 13,
+//       }}
+//       controller={true}
+//       layers={[new CartoLayer(cartoLayerConfig)]}
+//     />
+//   );
+// };
+
+// export default CartoLayerComponent;
+
+
+// import React from 'react';
+// import { setDefaultCredentials } from '@deck.gl/carto';
+// import { DeckGL } from '@deck.gl/react';
+// import { CartoLayer } from '@deck.gl/carto/typed';
+
+// const CARTO_USERNAME = 'attainutbs@gmail.com';
+//     const CARTO_API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhIjoiYWNfMWg2eGQxc2MiLCJqdGkiOiIwNDcxMjlkYiJ9.qdrpqYggDrYqaO1Oher5_lQn1pYy8TYdywexIm8cCGg';
+
+// setDefaultCredentials({
+//   apiKey: CARTO_API_KEY,
+//   username: CARTO_USERNAME,
+// });
+
+
+// const CartoLayerComponent = () => {
+//   const cartoLayerConfig = {
+//     data: 'cell_towers_worldwide', // Table name from Carto
+//     credentials: {
+//       serverUrl: 'https://YOUR_CARTO_USERNAME.carto.com', // Replace YOUR_CARTO_USERNAME with your Carto username
+//     },
+//     isVisible: true, // Set this to false if you want to hide the layer initially
+//   };
+
+//   return (
+//     <DeckGL
+//       initialViewState={{
+//         latitude: 40.7128,
+//         longitude: -74.0060,
+//         zoom: 13,
+//       }}
+//       controller={true}
+//       layers={[new CartoLayer(cartoLayerConfig)]}
+//     />
+//   );
+// };
+
+// export default CartoLayerComponent;
